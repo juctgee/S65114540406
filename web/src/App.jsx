7946b076f7d17fb96e46992bac58ebb2bb9966f1
Pach <phatchara.yu.65@ubu.ac.th>
2025-08-27 20:35:22 +0700

    Pass

---
+++ web/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        บางสิ่งบางอย่างในชีวิต ไม่ต้องดีที่สุด แค่อยู่ได้แบบไม่ทุกข์ ก็สุขใจแล้ว 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
